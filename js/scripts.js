a423b01ee5a42b7c9d466dcff821f0b8a5f66ad0
aren-a1212 <abdelazizahmed1212@gmail.com>
2024-10-20 08:15:40 -0500

    added comments within the js code to better understand what is happening.

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,4 @@ let PokemonList=[
     {name:'Lopunny', height: 3, types:['normal','poison'] },
     {name:'Abra', height: 2, types:['pyschic', 'sleeper']},
     {name:'Rapidash', height:5 , types:['fire','runner']}
-];
+]; //added in array and objects within my array
