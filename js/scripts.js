35d5f93ec63eaca707e520a6bedebbe71720c04b
aren-a1212 <abdelazizahmed1212@gmail.com>
2024-10-25 09:37:02 -0500

    adding a condtional.
    . The conditional should check if the height is above a certain value and display a message if it is or isn’t.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,27 @@
-let PokemonList=[
+
+// added in array and objects within my array
+let pokemonList=[
     {name:'Lopunny', height: 3, types:['normal','poison'] },
     {name:'Abra', height: 2, types:['pyschic', 'sleeper']},
     {name:'Rapidash', height:5 , types:['fire','runner']}
-]; //added in array and objects within my array
+];        
+
+//Use document.write() inside the loop’s code to write the Pokémon name on your website’s DOM.
+
+//for(let i= 0; i < pokemonList.length; i++){
+  //  document.write(
+  //` ${pokemonList[i].name} (height: ${pokemonList[i].height}) <br>`)
+    //}
+
+
+let message = "";
+
+for(let i= 0; i < pokemonList.length; i++){
+   if(pokemonList[i].height > 3){
+      message = "- Wow, that's big!";
+    } else if (pokemonList[i].height <= 3 ){
+       message = "- Wow thats tiny !";
+    }
+    document.write(
+       `${pokemonList[i].name} (height: ${pokemonList[i].height})  ${message}  <br>`);
+}
