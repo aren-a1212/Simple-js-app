d7897b11f1185b114399994164149c56eea5d60c
aren-a1212 <abdelazizahmed1212@gmail.com>
2024-11-01 18:33:52 -0500

    forEach() function instead of the for loop
     updating to forEach() function instead of the for loop , and commented out PART 2.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
 
 // added in array and objects within my array
+//let pokemonRepository = (function(){
 let pokemonList=[
     {name:'Lopunny', height: 3, types:['normal','poison'] },
     {name:'Abra', height: 2, types:['pyschic', 'sleeper']},
     {name:'Rapidash', height:5 , types:['fire','runner']}
-];        
+];  
+   //function add(pokemon){
+  //    pokemmonList.push(pokemon);
+  // }
+  // function getAll(){
+  //   return pokemonList;
+  // }
+ //  return{
+ //     add : add,
+ //     getAll : getAll
+ //  };
+//}  ) ();
+
 
 //Use document.write() inside the loop’s code to write the Pokémon name on your website’s DOM.
 
@@ -14,14 +27,26 @@ let pokemonList=[
     //}
 
 //  add a conditional. The conditional should check if the height is above a certain value
-let message = "";
+//let message = "";
 
-for(let i= 0; i < pokemonList.length; i++){
-   if(pokemonList[i].height > 3){
-      message = "- Wow, that's big!";
-    } else if (pokemonList[i].height <= 3 ){
-       message = "- Wow thats tiny !";
-    }
-    document.write(
-       `${pokemonList[i].name} (height: ${pokemonList[i].height})  ${message}  <br>`);
+//for(let i= 0; i < pokemonList.length; i++){
+ //  if(pokemonList[i].height > 3){
+ //     message = "- Wow, that's big!";
+  //  } else if (pokemonList[i].height <= 3 ){
+   //    message = "- Wow thats tiny !";
+  //  }
+   // document.write(
+    //   `${pokemonList[i].name} (height: ${pokemonList[i].height})  ${message}  <br>`);
+//}
+//PART 1 EX :1.5
+function myLoop(pokemon){
+   document.write('<p>' + pokemon.name + ' | height: ' + pokemon.height + '| type:  '  +  pokemon.types   + '</p>');
 }
+pokemonList.forEach(myLoop);
+
+
+
+//PART2
+//pokemonRepository.getAll().forEach(function (pokemon) {
+  //  document.write('<p>' + pokemon.name + ' | height: ' + pokemon.height + '</p>');
+//});
